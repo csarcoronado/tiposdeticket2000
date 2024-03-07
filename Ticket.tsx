4dde19e82f84b35187cbcfeb026e484362288f06
csarcoronado <146479049+csarcoronado@users.noreply.github.com>
2024-03-07 15:12:25 -0700

    Add files via upload

---
+++ Ticket.tsx
@@ -0,0 +1,222 @@
+import './Ticket.css'
+import './Ticketmediano.css'
+import './Ticketgrande.css' 
+const Ticket = () => {
+    return(
+        <>
+        <div className='division'>
+        <div className='ticke'>
+            <header className="ticket">
+            <img className="logo" src="img/logo.jpg" />
+            <section className="ticket">
+                <b>Matriz</b>
+                <h1 className="title">ESCUELA KEMPER URGATE</h1>
+                <br/>
+                <b>RFC</b>:EKU9003173C9
+                <br/>
+                AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br/>
+                111233123123 21312312312313
+                <br/>
+                <span>
+                <b>Sucursal</b>
+                </span>
+                <br/>
+                <b>RFC</b>:EKU9003173C9
+                <br/>
+                <b>LUGAR DE EXPEDICIÓN</b>: AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br/>
+                <br/>
+                111233123123 21312312312313
+                <br/>
+            </section>
+            <b>REGIMEN FISCAL</b>: 601 GENERAL DE LEY PERSONAS MORALES
+            <br />
+            <b>Folio</b>: NV 90000009
+            <br />
+            <b>Fecha</b>: 2023-06-01 12:21:03
+            <br />
+            </header>
+            <section className="ticket">
+            <hr />
+            <table className="tickett table-bordered">
+                <tr>
+                <td className="titulos-cabecera">CANT</td>
+                <td className="titulos-cabecera">PRODUCTO</td>
+                <td className="titulos-cabecera">PRECIO/U</td>
+                <td className="titulos-cabecera">SUBTOTAL</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETINAAAAAAAAAAAAAAAAAAAAAAAAAAA</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETIN</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+            </table>
+            <hr />
+            <h5 className="total">Total: $1,060.00MXN</h5>
+            </section>
+            <footer className="ticket">
+            <h3 className="reclamacion"> Cantidad de Productos: 1 </h3>
+            <p className="reclamacion"> PARA CUALQUIER RECLAMACION ES NECESARIO <br/> PRESENTAR SU TICKET DE COMPRA </p>
+            <p className="reclamacion">
+                <b>TELEFONO</b>: <b>6699259907</b>
+            </p>
+            <p className="reclamacion">
+                <strong>¡GRACIAS POR SU COMPRA!</strong>
+            </p>
+            </footer>
+            <script>{`window.print();`}</script>
+        </div>
+{/* ///////////////////////////////////Estilos Ticket mediano////////////////////////////////////////////////////////////////////////// */}
+        <div className='tickess'>
+            <header className="ticketett">
+            <img className="logo" src="img/logo.jpg" />
+            <section className="ticketett">
+                <b>Matriz</b>
+                <h1 className="title">ESCUELA KEMPER URGATE</h1>
+                <br />
+                <b>RFC</b>:EKU9003173C9
+                <br />
+                AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br />
+                111233123123 21312312312313
+                <br />
+                <span>
+                <b>Sucursal</b>
+                </span>
+                <br />
+                <b>RFC</b>:EKU9003173C9
+                <br />
+                <b>LUGAR DE EXPEDICIÓN</b>: AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br />
+                <br />
+                111233123123 21312312312313
+                <br />
+            </section>
+            <b>REGIMEN FISCAL</b>: 601 GENERAL DE LEY PERSONAS MORALES
+            <br />
+            <b>Folio</b>: NV 90000009
+            <br />
+            <b>Fecha</b>: 2023-06-01 12:21:03
+            <br />
+            </header>
+            <section className="ticketett">
+            <hr />
+            <table className="tickettst table-bordered">
+                <tr>
+                <td className="titulos-cabecera">CANT</td>
+                <td className="titulos-cabecera">PRODUCTO</td>
+                <td className="titulos-cabecera">PRECIO/U</td>
+                <td className="titulos-cabecera">SUBTOTAL</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETINAAAAAAAAAAAAAAAAAAAAAAAAAAA</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETIN</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+            </table>
+            <hr />
+            <h5 className="total">Total: $1,060.00MXN</h5>
+            </section>
+            <footer className="ticketett">
+            <h3 className="reclamacion"> Cantidad de Productos: 1 </h3>
+            <p className="reclamacion"> PARA CUALQUIER RECLAMACION ES NECESARIO <br/> PRESENTAR SU TICKET DE COMPRA </p>
+            <p className="reclamacion">
+                <b>TELEFONO</b>: <b>6699259907</b>
+            </p>
+            <p className="reclamacion">
+                <strong>¡GRACIAS POR SU COMPRA!</strong>
+            </p>
+            </footer>
+            <script>{`window.print();`}</script>
+        </div>
+{/* /////////////////////////////////////Estilos Ticket grande//////////////////////////////////////////////////////////////////////// */}
+        <div className='tickes'>
+            <header className="ticketet">
+            <img className="logo" src="img/logo.jpg" />
+            <section className="ticketet">
+                <b>Matriz</b>
+                <h1 className="title">ESCUELA KEMPER URGATE</h1>
+                <br />
+                <b>RFC</b>:EKU9003173C9
+                <br />
+                AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br />
+                111233123123 21312312312313
+                <br />
+                <span>
+                <b>Sucursal</b>
+                </span>
+                <br />
+                <b>RFC</b>:EKU9003173C9
+                <br />
+                <b>LUGAR DE EXPEDICIÓN</b>: AV. MENDOZA 456 GARITA DE OTAY OTRA COLONIA MUY LARGA 20928 Baja California TIJUANA
+                <br />
+                <br />
+                111233123123 21312312312313
+                <br />
+            </section>
+            <b>REGIMEN FISCAL</b>: 601 GENERAL DE LEY PERSONAS MORALES
+            <br />
+            <b>Folio</b>: NV 90000009
+            <br />
+            <b>Fecha</b>: 2023-06-01 12:21:03
+            <br />
+            </header>
+            <section className="ticketet">
+            <hr />
+            <table className="ticketts table-bordered">
+                <tr>
+                <td className="titulos-cabecera">CANT</td>
+                <td className="titulos-cabecera">PRODUCTO</td>
+                <td className="titulos-cabecera">PRECIO/U</td>
+                <td className="titulos-cabecera">SUBTOTAL</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETINAAAAAAAAAAAAAAAAAAAAAAAAAAA</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+                <tr>
+                <td>1.00</td>
+                <td>MALETIN</td>
+                <td>$1,160.00</td>
+                <td>$1,160.00</td>
+                </tr>
+            </table>
+            <hr />
+            <h5 className="total">Total: $1,060.00MXN</h5>
+            </section>
+            <footer className="ticketet">
+            <h3 className="reclamacion"> Cantidad de Productos: 1 </h3>
+            <p className="reclamacion"> PARA CUALQUIER RECLAMACION ES NECESARIO <br/> PRESENTAR SU TICKET DE COMPRA </p>
+            <p className="reclamacion">
+                <b>TELEFONO</b>: <b>6699259907</b>
+            </p>
+            <p className="reclamacion">
+                <strong>¡GRACIAS POR SU COMPRA!</strong>
+            </p>
+            </footer>
+            <script>{`window.print();`}</script>
+        </div>
+        </div>
+
+        </>
+    )
+}
+export default Ticket
